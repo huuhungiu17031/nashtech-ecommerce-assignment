bd617fecc0ea014963cf94e087f6786f36ac76db
NguyenHuuHung <nguyenhuuhung.9a10.2013@gmail.com>
2024-05-16 09:12:30 +0700

    change cors config

--- src/main/java/com/nashtech/cellphonesfake/configuration/CorsConfig.java
+++ src/main/java/com/nashtech/cellphonesfake/configuration/CorsConfig.java
@@ -1,13 +1,23 @@
 package com.nashtech.cellphonesfake.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CorsConfig {
+    @Value("${user_origin}")
+    private String userOrigin;
+    @Value("${admin_origin}")
+    private String adminOrigin;
+
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:5173"); // Allow requests from this ori
+        List<String> origins = Arrays.asList(userOrigin, adminOrigin);
+        corsConfiguration.setAllowedOrigins(origins);
         corsConfiguration.addAllowedHeader("*"); // Allow all headers
         corsConfiguration.addAllowedMethod("*"); // Allow all HTTP methods
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
